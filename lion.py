08e7a57e7d779f6b738c4370428ab238358a8bce
Admin <admin@Admins-MacBook-Pro.local>
2024-02-26 14:52:11 +0530

    lion adding2

---
+++ lion.py
@@ -0,0 +1,12 @@
+# class Animal:
+#     def eat(self):
+#         print("I can Eat")
+
+#     def sleep(self):
+#         print("I can Sleep")
+
+#     def sound(self):
+#         print("I can Sound")
+
+#     def run(self):
+#         print("I can Run")
